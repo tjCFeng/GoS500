fa76a75868d5b744e88480dbc88c5c48a60fe14c
tjCFeng, LiuYang <tjCFeng@163.com>
2016-02-06 15:30:08 +0800

    Update GoS500.go

--- GoS500.go
+++ GoS500.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"os"
 	"github.com/tjCFeng/GoS500/S500"
 )
 
@@ -18,4 +20,22 @@ func main() {
 	defer S500.FreeGPIO(PB31)
 	PB31.SetFun(S500.FunOUT)
 	PB31.Flip()
+	
+	PB31, _ := S500.CreateGPIO(S500.PB, 31)
+	PWM3, _ := S500.CreatePWM(PB31, S500.PWM_3)
+	defer S500.FreePWM(PWM3)
+	PWM3.SetSRC(S500.PWM_HOSC_24M)
+	PWM3.SetDIV(0xFF)
+	PWM3.SetPolarity(true)
+	PWM3.SetPeriod(1000)
+	PWM3.SetDuty(500)
+	
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		key, _, _:= reader.ReadLine()
+		switch string(key)  {
+			case "exit": return
+			default: continue
+		}
+	}
 }
